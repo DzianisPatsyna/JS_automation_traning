fca144a0aa8653bae14ef02a281e9af5da44facc
Patsyna, Dzianis <dzianis_patsyna@epam.com>
2018-05-14 14:20:06 +0300

    code refactored

--- tasks/multiplyInArray.js
+++ tasks/multiplyInArray.js
@@ -1,7 +1,6 @@
 const array = new Array(10)
     .fill(0)
-    .map(a => a = Math.round(Math.random() * 10) + 1)
+    .map(a => Math.round(Math.random() * 10) + 1)
     .sort((a, b) => b - a)
-    .splice(-3)
-
-const multiply = array.reduce((acum, curr) => acum * curr);
+    .splice(0, 3)
+    .reduce((acum, curr) => acum * curr);
