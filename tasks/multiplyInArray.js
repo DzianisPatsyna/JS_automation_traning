96d362369342ee96a5aafa3d2bfde115eb132cde
Patsyna, Dzianis <dzianis_patsyna@epam.com>
2018-05-14 13:58:13 +0300

    changed code structure by review

--- tasks/multiplyInArray.js
+++ tasks/multiplyInArray.js
@@ -1,9 +1,7 @@
-const array = [];
-
-for (let i = 0; i < 10; i++) {
-    array.push(Math.round(Math.random() * 10) + 1);
-}
-
-array.sort((a, b) => b - a).splice(3)
+const array = new Array(10)
+    .fill(0)
+    .map(a => a = Math.round(Math.random() * 10) + 1)
+    .sort((a, b) => b - a)
+    .splice(-3)
 
 const multiply = array.reduce((acum, curr) => acum * curr);
