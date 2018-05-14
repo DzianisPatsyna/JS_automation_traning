e87174c60612e75d71c819a990ffbd54beb1a4ab
Patsyna, Dzianis <dzianis_patsyna@epam.com>
2018-05-14 16:32:22 +0300

    changes by review

--- tasks/time.js
+++ tasks/time.js
@@ -1,2 +1,4 @@
-const now = new Date()
-const seconds = now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+const getSeconds = () => {
+    const now = new Date();
+    return now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+}
